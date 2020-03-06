840f1273d7472494ef11da70264763cb1ff27c7c
Matt McCoy <mccoym3@wit.edu>
2020-03-06 12:15:10 -0500

    added settings class made some changes to main(unsure what exactly)

--- src/edu/wit/cs/dyermccoy/agario/Main.java
+++ src/edu/wit/cs/dyermccoy/agario/Main.java
@@ -3,7 +3,7 @@ package edu.wit.cs.dyermccoy.agario;
 import java.util.ArrayList;
 
 import javafx.application.Application;
-
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -15,38 +15,47 @@ public class Main extends Application{
 	ArrayList<Circle> cells = new ArrayList<>();
 	
 	//static variables controlling width height and the amount of food spawned
-	static int windowWidth = 1280;
-	static int windowHeight = 720;
-	static int limitFood = 50;
+	
 	
 		@Override
 		public void start(Stage primaryStage) {
-			
+			double pointerX = 0;
+		double 	pointerY= 0;
 			try {
+				Circle pointer = new Circle(pointerX, pointerY, 0);
 				//user circle is created
 				Circle UserDot = new Circle(100, 100, 30, Color.BLUE);
 				cells.add(UserDot);
 				//user circle follows mouse only when mouse is inside dot
-				UserDot.setOnMouseMoved(e -> {
-				
-					UserDot.setCenterX(e.getX());
-					UserDot.setCenterY(e.getY());
+				Pane f = new Pane();
+				f.setOnMouseMoved(e -> {
+					
+					pointer.setCenterX(e.getX());
+					pointer.setCenterY(e.getY());
+					
+					
+					Point2D p = new Point2D(pointer.getCenterX(), pointer.getCenterY());
+					Point2D u = new Point2D(UserDot.getCenterX(), UserDot.getCenterY());
+					double angleUToP = u.angle(p);
+					
 					
 				});
+				
+			
+                
 				//food is spawned & added to dot ArrayList
-				for(int i = 0 ; i < limitFood; i++) {
-					new Food(windowWidth, windowHeight);
+				for(int i = 0 ; i < Settings.limitFood; i++) {
+					new Food(Settings.windowWidth, Settings.windowHeight);
 				}
 				
 				
-			
-				Pane f = new Pane();
-				
+
+				f.getChildren().add(pointer);
 				f.getChildren().addAll(Food.foodObjects);
-				f.getChildren().addAll(cells);
+				f.getChildren().addAll(UserDot);
 				
 				
-				Scene scene = new Scene(f,windowWidth,windowHeight);
+				Scene scene = new Scene(f,Settings.windowWidth,Settings.windowHeight);
 				
 				
 				primaryStage.setScene(scene);
---
+++ src/edu/wit/cs/dyermccoy/agario/Settings.java
@@ -0,0 +1,9 @@
+package edu.wit.cs.dyermccoy.agario;
+
+public class Settings {
+
+	static int windowWidth = 1280;
+	static int windowHeight = 720;
+	static int limitFood = 50;
+	
+}
